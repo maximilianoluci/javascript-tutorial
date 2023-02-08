58f88f05445d7a630103ba45ca6987e1333c4ba1
maxi <maximiliano.luci@icloud.com>
2023-02-08 13:53:17 +0100

    finished section 6

---
+++ s6/v64/app.js
@@ -0,0 +1,12 @@
+var person = {
+  firstname: "Default",
+  lastname: "Default",
+  greet: function () {
+    return "Hi " + this.firstname;
+  },
+};
+
+var john = Object.create(person);
+john.firstname = "John";
+john.lastname = "Doe";
+console.log(john);
