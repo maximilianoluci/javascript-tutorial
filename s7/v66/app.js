a01353daae3da95cd7ddbd7e939b7ef488b08547
maxi <maximiliano.luci@icloud.com>
2023-02-09 14:28:47 +0100

    tyjh

---
+++ s7/v66/app.js
@@ -0,0 +1,17 @@
+var people = [
+  {
+    firstname: "John",
+    lastname: "Doe",
+    addresses: ["111 Main Street", "222 Third Street"],
+  },
+  {
+    firstname: "Jane",
+    lastname: "Doe",
+    addresses: ["222 Main Street", "444 Fifth Street"],
+    greet: function () {
+      return "Hello!";
+    },
+  },
+];
+
+console.log(people);
