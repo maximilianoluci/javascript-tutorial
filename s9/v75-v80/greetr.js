2d2062c65db852e0c005158b614aed924fb0a7a3
Maximiliano Luci <maximiliano.luci@icloud.com>
2023-02-18 15:53:59 +0100

    e

--- s9/v75-v80/greetr.js
+++ s9/v75-v80/greetr.js
@@ -2,4 +2,15 @@
   var Greetr = function (firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   };
+
+  Greetr.init = function (firstName, lastName, language) {
+    var self = this;
+    firstName = firstName || "";
+    lastName = lastName || "";
+    language = language || "en";
+  };
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
 })(window, jQuery);
