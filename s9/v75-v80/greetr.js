14c707ae78fef42d2996d401bb4bd20b9e3cdb4f
Maximiliano Luci <maximiliano.luci@icloud.com>
2023-03-06 14:22:46 +0100

    Update greetr.js

--- s9/v75-v80/greetr.js
+++ s9/v75-v80/greetr.js
@@ -3,6 +3,43 @@
     return new Greetr.init(firstName, lastName, language);
   };
 
+  var supportedLangs = ["en", "es"];
+
+  var greetings = {
+    en: "Hello",
+    es: "Hola",
+  };
+
+  var formalGreetings = {
+    en: "Greetings",
+    es: "Saludos",
+  };
+
+  var loggedMessages = {
+    en: "Logged in",
+    es: "Sesión iniciada",
+  };
+
+  Greetr.prototype = {
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    },
+
+    validate: function () {
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw new Error("Invalid language");
+      }
+    },
+
+    greeting: function () {
+      return greetings[this.language] + " " + this.firstName + "!";
+    },
+
+    formalGreeting: function () {
+      return formalGreetings[this.language] + ", " + this.fullName();
+    },
+  };
+
   Greetr.init = function (firstName, lastName, language) {
     var self = this;
     firstName = firstName || "";
